361570421633579ed27080b134c089dc65295b9f
Nicolás Butterfield <nicobutter@gmail.com>
2024-06-08 17:04:26 -0300

    ugprade_login_register

--- relevamiento_urbanizacion/accounts/views.py
+++ relevamiento_urbanizacion/accounts/views.py
@@ -1,3 +1,27 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import login, authenticate
+from .forms import RegisterForm
 
-# Create your views here.
+def register_view(request):
+    if request.method == 'POST':
+        form = RegisterForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('home')  # Asegúrate de tener una vista 'home' configurada
+    else:
+        form = RegisterForm()
+    return render(request, 'accounts/register.html', {'form': form})
+
+def login_view(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('home')
+        else:
+            # Maneja el caso de usuario no autenticado
+            ...
+    return render(request, 'accounts/login.html')
