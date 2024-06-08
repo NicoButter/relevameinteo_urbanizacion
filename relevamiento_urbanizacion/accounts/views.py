3238988f12adb092c0d2501455e8a93198c4813b
Nicolás Butterfield <nicobutter@gmail.com>
2024-06-08 19:41:03 -0300

    correciones en login_faltaMejorarLosEstilos

--- relevamiento_urbanizacion/accounts/views.py
+++ relevamiento_urbanizacion/accounts/views.py
@@ -1,17 +1,18 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import login, authenticate
-from .forms import RegisterForm
+from django.contrib import messages
+#from .forms import RegisterForm
 
-def register_view(request):
-    if request.method == 'POST':
-        form = RegisterForm(request.POST)
-        if form.is_valid():
-            user = form.save()
-            login(request, user)
-            return redirect('home')  # Asegúrate de tener una vista 'home' configurada
-    else:
-        form = RegisterForm()
-    return render(request, 'accounts/register.html', {'form': form})
+# def register_view(request):
+#     if request.method == 'POST':
+#         form = RegisterForm(request.POST)
+#         if form.is_valid():
+#             user = form.save()
+#             login(request, user)
+#             return redirect('home')  # Asegúrate de tener una vista 'home' configurada
+#     else:
+#         form = RegisterForm()
+#     return render(request, 'accounts/register.html', {'form': form})
 
 def login_view(request):
     if request.method == 'POST':
@@ -20,8 +21,10 @@ def login_view(request):
         user = authenticate(request, username=username, password=password)
         if user is not None:
             login(request, user)
-            return redirect('home')
+            return redirect('main')
         else:
-            # Maneja el caso de usuario no autenticado
-            ...
-    return render(request, 'accounts/login.html')
+            # mensaje de error
+            messages.error(request, 'Usuario no registrado. Si es un error por favor, comuníquese con el servicio de atención al cliente.')
+            return render(request, 'accounts/login.html')
+
+    return render(request, 'accounts/main.html')
