1db67066790446deff02e8b8f490a06a76db1f00
Nicolás Butterfield <nicobutter@gmail.com>
2024-06-08 18:21:04 -0300

    pirmera_migracion

--- relevamiento_urbanizacion/relevamiento_urbanizacion/settings.py
+++ relevamiento_urbanizacion/relevamiento_urbanizacion/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'relevamiento_urbanizacion.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'urbanizacion_db',
+        'USER': 'franco',
+        'PASSWORD': 'franco010203',
+        'HOST': 'localhost', 
+        'PORT': '5432',
     }
 }
 
@@ -112,11 +116,18 @@ USE_I18N = True
 
 USE_TZ = True
 
-
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
